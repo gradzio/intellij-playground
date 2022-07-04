a1e32aecb028c289c4853f9efc013b4971e09a96
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2022-07-04 16:44:41 +0200

    Configure launching CLI script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,66 @@
 plugins {
-  id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.6.20"
-  id("org.jetbrains.intellij") version "1.5.2"
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.6.20"
+    id("org.jetbrains.intellij") version "1.5.2"
 }
 
 group = "com.lowgular.intellij.plugin"
 version = "1.0-SNAPSHOT"
 
 repositories {
-  mavenCentral()
+    mavenCentral()
+}
+
+sourceSets {
+    create("unpacked") {
+        resources {
+            setSrcDirs(listOf("gen/main/unpacked"))
+        }
+    }
 }
 
 // Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-  version.set("2022.1")
-  type.set("IU") // Target IDE Platform
+    version.set("2022.1")
+    type.set("IU") // Target IDE Platform
 
-  plugins.set(listOf("JavaScriptLanguage"))
+    plugins.set(listOf("JavaScriptLanguage"))
 }
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "11"
-    targetCompatibility = "11"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
-  }
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
 
-  patchPluginXml {
-    sinceBuild.set("212")
-    untilBuild.set("222.*")
-  }
+    buildPlugin {
+        from(sourceSets.named("unpacked").get().allSource) {
+            into("")
+        }
+    }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-  }
+    prepareSandbox {
+        from(sourceSets.named("unpacked").get().allSource) {
+            into(pluginName.get())
+        }
+    }
 
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
-  }
+    patchPluginXml {
+        sinceBuild.set("212")
+        untilBuild.set("222.*")
+    }
+
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
 }
---
+++ src/main/kotlin/com/lowgular/intellij/actions/CreateDTO.kt
@@ -0,0 +1,141 @@
+package com.lowgular.intellij.actions
+
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.process.CapturingProcessHandler
+import com.intellij.lang.javascript.service.JSLanguageServiceUtil
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.Attachment
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.ui.Messages
+import com.intellij.openapi.util.text.StringUtil
+import org.codehaus.jettison.json.JSONObject
+import org.codehaus.jettison.json.JSONTokener
+import java.io.File
+
+private val LOG: Logger = Logger.getInstance("#org.lowgular")
+private var myLogErrors: ThreadLocal<Boolean> = ThreadLocal.withInitial { true }
+
+private fun shortenOutput(output: String): String {
+    return StringUtil.shortenTextWithEllipsis(
+        output.replace('\\', '/')
+            .replace("(/[^()/:]+)+(/[^()/:]+)(/[^()/:]+)".toRegex(), "/...$1$2$3"),
+        750, 0
+    )
+}
+
+fun makeCommand(pluginDir: String): GeneralCommandLine {
+    val cliFile = JSLanguageServiceUtil.getPluginDirectory(CreateDTO::class.java, "cli/api-client.cli.js")
+    LOG.warn(cliFile.absolutePath);
+    val moduleExe = "node"
+    val commandLine = GeneralCommandLine(
+        moduleExe, cliFile.toString(), "{\n" +
+                "    smartComponent: 'Component',\n" +
+                "    resolver: 'Resolver',\n" +
+                "    directive: 'Directive',\n" +
+                "    guard: 'Guard',\n" +
+                "    eventHandler: 'EventHandler',\n" +
+                "    dto: 'DTO',\n" +
+                "    dtoPort: 'DtoPort',\n" +
+                "    command: 'Command',\n" +
+                "    commandPort: 'CommandPort',\n" +
+                "    query: 'Query',\n" +
+                "    queryPort: 'QueryPort',\n" +
+                "    context: 'Context',\n" +
+                "    contextPort: 'ContextPort',\n" +
+                "    dtoService: 'Service',\n" +
+                "    responseObject: 'Response',\n" +
+                "    storage: 'Storage',\n" +
+                "    state: 'State',\n" +
+                "    page: 'Page',\n" +
+                "  }"
+    )
+
+    return commandLine
+}
+
+fun grabCommandOutput(commandLine: GeneralCommandLine): String {
+//  val moduleExe = "$modulePath${File.separator}bin${File.separator}ng
+
+//  if (filePath != null) {
+//    commandLine.withWorkDirectory(filePath)
+//  }
+    val handler = CapturingProcessHandler(commandLine)
+    val output = handler.runProcess()
+
+    if (output.exitCode == 0) {
+        if (output.stderr.trim().isNotEmpty()) {
+            if (myLogErrors.get()) {
+                LOG.error(
+                    "Error while loading schematics info.\n"
+                            + shortenOutput(output.stderr),
+                    Attachment("err-output", output.stderr)
+                )
+            } else {
+                LOG.info(
+                    "Error while loading schematics info.\n"
+                            + shortenOutput(output.stderr)
+                )
+            }
+        }
+        return output.stdout
+    } else if (myLogErrors.get()) {
+        LOG.error(
+            "Failed to load schematics info.\n"
+                    + shortenOutput(output.stderr),
+            Attachment("err-output", output.stderr),
+            Attachment("std-output", output.stdout)
+        )
+        throw RuntimeException(
+            "Failed to load schematics info.\n"
+                    + shortenOutput(output.stderr)
+        )
+    } else {
+        LOG.info(
+            "Error while loading schematics info.\n"
+                    + shortenOutput(output.stderr)
+        )
+    }
+    throw RuntimeException(
+        "Failed to load schematics info.\n"
+                + shortenOutput(output.stderr)
+    )
+}
+
+class CreateDTO : DumbAwareAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project = e.project ?: return
+        val file = e.getData(PlatformDataKeys.VIRTUAL_FILE) ?: return
+        LOG.warn("${project.basePath} or ${project.projectFilePath}")
+//    val files = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
+        val txt = Messages.showInputDialog(
+            project, "What is the DTO name?",
+            "What is the DTO name?", Messages.getQuestionIcon()
+        )
+
+        val application = ApplicationManager.getApplication()
+        application.executeOnPooledThread {
+            //    val result = "node /home/ortho/Desktop/projects/gradzio/nx-test/test.js".runCommand()
+            val result = grabCommandOutput(makeCommand("test"));
+            val data = JSONTokener(result).nextValue() as JSONObject
+
+            application.invokeLater {
+                if (data.getString("status") == "error") {
+                    Messages.showErrorDialog(project, data.getString("message"), "Error");
+                }
+                if (data.getString("status") == "ok") {
+                    Messages.showMessageDialog(
+                        project,
+                        data.getString("message"),
+                        "Done",
+                        Messages.getInformationIcon()
+                    )
+                }
+            }
+        }
+
+
+    }
+}
